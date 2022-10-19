db0d54638ab657a41a03643fd7efec7a0d13ab7f
jump_jirasak <jump.jjnn@gmail.com>
2022-10-19 13:53:16 +0700

    ep.35_form_of_databinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {
+  NgbAlertModule,
+  NgbModalConfig,
+  NgbModule,
+  NgbPaginationModule,
+} from '@ng-bootstrap/ng-bootstrap';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ServerComponent, ServersComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbPaginationModule,
+    NgbModule,
+    NgbAlertModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [NgbModalConfig],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
